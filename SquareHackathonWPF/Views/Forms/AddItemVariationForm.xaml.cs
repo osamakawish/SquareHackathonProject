9b2e24420c701a1b42e814c258a76cb0e4274053
Osama Kawish <osamakawish@gmail.com>
2023-05-05 15:34:25 -0400

    Pricing moved to item variation additions. Item variation additions are moved to their own window. They can also be edited.

---
+++ SquareHackathonWPF/Views/Forms/AddItemVariationForm.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Square.Models;
+
+namespace SquareHackathonWPF.Views.Forms
+{
+    public enum PricingType
+    {
+        Fixed,
+        Variable
+    }
+
+    /// <summary>
+    /// Interaction logic for AddIemVariationForm.xaml
+    /// </summary>
+    public partial class AddItemVariationForm : Window
+    {
+        internal bool        IsEditing       { get; init; } = false;
+        internal string      ItemId          { get; set; } = "";
+        internal string      VariationId     { get; set; } = "";
+        internal string      VariationName   { get; set; } = "";
+        internal PricingType PricingType     { get; set; }
+        internal string      PricingValue    { get; set; } = "";
+        internal string      PricingCurrency { get; set; } = "";
+
+        public AddItemVariationForm()
+        {
+            InitializeComponent();
+
+            Closing += OnFormClosing;
+        }
+
+        private void OnFormClosing(object? _, CancelEventArgs args) => args.Cancel = !InputsAreValid();
+
+        private bool InputsAreValid()
+        {
+            if (string.IsNullOrWhiteSpace(ItemId)) {
+                WarningTextBlock.Text = "Item ID cannot be empty";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(VariationNameTextBox.Text)) {
+                WarningTextBlock.Text = "Variation name cannot be empty";
+                return false;
+            }
+
+            switch (PricingTypeComboBox.SelectedIndex) {
+                case -1 or > 1:
+                    WarningTextBlock.Text = "Pricing type must be selected";
+                    return false;
+                case 0 when string.IsNullOrWhiteSpace(PricingValueTextBox.Text):
+                    WarningTextBlock.Text = "Pricing value cannot be empty";
+                    return false;
+                case 0 when string.IsNullOrWhiteSpace(PricingCurrencyTextBox.Text):
+                    WarningTextBlock.Text = "Currency cannot be empty";
+                    return false;
+                default:
+                    // Too much work to validate the pricing value and currency. Let the API do it.
+
+                    return true;
+            }
+        }
+
+        private void PricingTypeSelected(object sender, RoutedEventArgs e)
+        {
+            switch (PricingTypeComboBox.SelectedIndex) {
+                case 0:
+                    PricingValueTextBox.Visibility = Visibility.Visible;
+                    PricingCurrencyTextBox.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    PricingValueTextBox.Visibility = Visibility.Hidden;
+                    PricingCurrencyTextBox.Visibility = Visibility.Hidden;
+                    break;
+            }
+        }
+
+        internal void GetVariation(out CatalogItemVariation variation, out CatalogObject variationAsCatalogObject)
+        {
+            var variationBuilder = new CatalogItemVariation.Builder()
+                .ItemId(ItemId)
+                .Name(VariationNameTextBox.Text);
+
+            variationBuilder.PricingType(PricingTypeComboBox.SelectedIndex switch
+            {
+                0 => "FIXED_PRICING",
+                1 => "VARIABLE_PRICING",
+                _ => throw new InvalidOperationException("Invalid pricing type")
+            });
+
+            switch (PricingTypeComboBox.SelectedIndex) {
+                case 0 when long.TryParse(PricingValueTextBox.Text, out var price):
+                    variationBuilder.PriceMoney(new Money.Builder()
+                        .Amount(price)
+                        .Currency(PricingCurrencyTextBox.Text)
+                        .Build());
+                    break;
+                case 0:
+                    WarningTextBlock.Text = "Invalid price. Price must be an integer multiple of lowest unit of currency.";
+                    break;
+            }
+
+            variation = variationBuilder.Build();
+
+            variationAsCatalogObject = new CatalogObject.Builder("ITEM_VARIATION", VariationIdTextBox.Text)
+                .ItemVariationData(variation)
+                .Build();
+        }
+    }
+}
--- SquareHackathonWPF/Views/Forms/AddItemWindow.xaml.cs
+++ SquareHackathonWPF/Views/Forms/AddItemWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Square.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,33 +27,60 @@ namespace SquareHackathonWPF.Views.Forms
 
         private void AddVariationButtonClick(object sender, RoutedEventArgs e)
         {
-            var idForm = new TextBox
-            {
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Padding = new(3),
-                MaxLines = 1,
-                MinWidth = 128,
-                Background = Brushes.LightGray,
-                Margin = new(3),
-                Text = "Id.",
-                Tag = "VariationId"
+            var window = new AddItemVariationForm { ItemId = ItemIdTextBox.Text };
+            window.ShowDialog();
+
+            window.Closed += delegate {
+                window.GetVariation(out var variation, out var variationAsCatalogObject);
+                AddVariation(variation, variationAsCatalogObject);
+            };
+        }
+
+        private void AddVariation(CatalogItemVariation variation, CatalogObject variationAsCatalogObject)
+        {
+            var style = new Style(typeof(TextBlock)) {
+                Setters = {
+                    new Setter(HorizontalAlignmentProperty, HorizontalAlignment.Left),
+                    new Setter(PaddingProperty, new Thickness(3)),
+                    new Setter(WidthProperty, 112),
+                    new Setter(ForegroundProperty, Brushes.LightGray),
+                    new Setter(MarginProperty, new Thickness(3))
+                }
             };
 
-            var nameForm = new TextBox
+            var button = new Button
             {
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Padding = new(3),
-                MaxLines = 1,
-                MinWidth = 128,
-                Background = Brushes.LightGray,
-                Margin = new(3),
-                Text = "Name.",
-                Tag = "VariationName"
+                Width = 36,
+                Background = Brushes.Transparent,
+                BorderThickness = new (0),
+                Content = new TextBlock
+                {
+                    TextDecorations = TextDecorations.Underline,
+                    Foreground = Brushes.DeepSkyBlue,
+                    Width = 32,
+                    Text = "Edit."
+                }
             };
 
-            var stackPanel = new StackPanel { Children = { idForm, nameForm } };
+            var idBlock = new TextBox { Text = variationAsCatalogObject.Id, Tag = "VariationId" };
 
-            FormPanel.Children.Add(stackPanel);
+            var nameBlock = new TextBox { Text = variation.Name, Tag = "VariationName" };
+
+            var pricingBlock = new TextBox
+            {
+                Text = $"{variation.PriceMoney.Amount} ({variation.PriceMoney.Currency})",
+                Width = 96, Foreground = Brushes.MediumSeaGreen,
+                Tag = "VariationPricing"
+            };
+
+            var stackPanel = new StackPanel
+            {
+                Resources = new() { { typeof(TextBlock), style } },
+                Orientation = Orientation.Horizontal,
+                Children = { button, idBlock, nameBlock, pricingBlock }
+            };
+
+            VariationsStackPanel.Children.Add(stackPanel);
         }
 
         private void OkButtonClick(object sender, RoutedEventArgs e)
@@ -60,6 +88,33 @@ namespace SquareHackathonWPF.Views.Forms
             // try to add the item
 
             // if it fails, show the error message, ideally the same one produced by the square api
+        }
+
+        private void ClickEditButton(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var stackPanel = (StackPanel)button.Parent;
+
+            var idBlock = (TextBox)stackPanel.Children[1];
+            var nameBlock = (TextBox)stackPanel.Children[2];
+            var pricingBlock = (TextBox)stackPanel.Children[3];
+
+            var variationForm = new AddItemVariationForm {
+                IsEditing = true,
+                ItemId = ItemIdTextBox.Text,
+                VariationId = idBlock.Text,
+                VariationName = nameBlock.Text,
+            };
+
+            if (pricingBlock.Text == "Price Varies") {
+                variationForm.PricingType = PricingType.Variable;
+            }
+            else {
+                var pricing = pricingBlock.Text.Split(' ');
+                variationForm.PricingType = PricingType.Fixed;
+                variationForm.PricingValue = long.Parse(pricing[0]).ToString();
+                variationForm.PricingCurrency = pricing[1].Trim('(', ')');
+            }
         }
     }
 }
